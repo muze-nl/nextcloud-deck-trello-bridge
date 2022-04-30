0d2af0b969fe9cbfe8c70b6a3d2446c8005472ca
Ben Peachey <ben@muze.nl>
2022-04-30 13:03:56 +0200

    Add tests for JS `clone()` function.

--- tests/js/module.functions.js
+++ tests/js/module.functions.js
@@ -5,4 +5,65 @@ QUnit.module('Functions', function () {
     assert.equal(typeof self.setItem, 'function', 'function self.setItem should exist')
     assert.equal(typeof self.getItem, 'function', 'function self.getItem should exist')
   });
+
+  QUnit.module('clone()', function () {
+    QUnit.test('error without parameters', function (assert) {
+      assert.throws(
+        function () {
+          clone()
+        },
+        new SyntaxError('JSON.parse: unexpected character at line 1 column 1 of the JSON data'),
+        'clone() should throw an error when not given any parameters'
+      );
+    })
+
+    QUnit.test('error with undefined', function (assert) {
+      assert.throws(
+        function () {
+          clone(undefined)
+        },
+        new SyntaxError('JSON.parse: unexpected character at line 1 column 1 of the JSON data'),
+        'clone() should throw an error when given undefined'
+      );
+    })
+
+    QUnit.test('error with function', function (assert) {
+      assert.throws(
+        function () {
+          clone(function () {})
+        },
+        new SyntaxError('JSON.parse: unexpected character at line 1 column 1 of the JSON data'),
+        'clone() should throw an error when given undefined'
+      );
+    })
+
+    QUnit.test.each('return value matches input', {
+        "true": true,
+        "false": false,
+        "null": null,
+        "int:0": 0,
+        "int:1": 1,
+        "string:empty": '',
+        "string:foo": 'foo',
+        "array:empty": [],
+        "array": ['foo', 'bar'],
+        "object:empty": {},
+        "object:simple": {foo: 'bar'},
+        "object:complex": mockBoard,
+      },
+      function (assert, value) {
+        assert.deepEqual(clone(value), value, 'clone() should return same value as given when given a parameter')
+      }
+    );
+
+    QUnit.test('return a clone', function (assert) {
+      const original = {foo: 'bar'}
+
+      const actual = clone(original);
+
+      original.foo = 'baz';
+
+      assert.deepEqual(actual, {foo: 'bar'}, 'clone() should return a clone of the given object when called with an object')
+    });
+  })
 });
